35307ae681e4f299c6f7635a2ac6319e6d1a90b3
Wojciech Rogóż <wrogoz@gmail.com>
2018-11-04 21:39:21 +0100

    ukończono sekcję z kalkulatorem

--- js/main.js
+++ js/main.js
@@ -31,11 +31,10 @@ backward.addEventListener('click', () => {
 
 //calc section
 
-/* to DO:
-    zrobic autozamykanie otwartych li przy ponownym clicku
-    poprawic css
-    dodac osobno dopisanie transportu na checked!
-    */
+// to DO:
+   
+//     poprawic css
+
 
 //DOM
 const arrows = document.querySelectorAll('.list_arrow');
@@ -47,6 +46,12 @@ const panelTransport = document.querySelector('span.transport');
 const colorFromListVal = document.querySelector('span.color.value');
 const patternFromListVal = document.querySelector('span.pattern.value');
 const materialFromListVal = document.querySelector('span.material.value');
+const transportVal = document.querySelector('span.transport.value');
+const transport=document.querySelector('#transport');
+const transportPanelL=document.querySelector('span.transport');
+const values=document.querySelectorAll('.value');
+let finalResult=document.querySelector('div.sum strong');
+
 //Objects
 let patternOfChair={
     Clair:500,
@@ -55,17 +60,25 @@ let patternOfChair={
 }
 let colorOfChair={
     Czerwony:0,
-    Czarny:400
+    Czarny:400,
+    Pomarańczowy:300
 }
 let materialOfChair={
     Tkanina:0,
     Skóra:1200
 }
-let transportCost=52;
-
+const transportCost = 200;
+//functions
+function sumAll(arr,displayBox){
+    let result=0;
+    for(let i=0; i<arr.length;i++){
+        arr[i].innerText == '' ? result += 0 : result += parseInt(arr[i].innerText);
+        }
+    return displayBox.innerText=result;
+}
+//events
     for(let i=0; i<arrows.length;i++){
         arrows[i].addEventListener('click', ()=>{
-            // tutaj wpisac jesli kliknalem inny element reszta wywala klase showhidden
         arrows[i].nextElementSibling.classList.contains('showHidden')?
        arrows[i].nextElementSibling.classList.remove('showHidden')
                 : arrows[i].nextElementSibling.classList.add('showHidden')
@@ -74,7 +87,6 @@ let transportCost=52;
 
 for (let i = 0; i < calcList.length; i++) {
     calcList[i].addEventListener('click',()=>{
-        console.log( 'item-'+i+' clicked')
         if (calcList[i].parentElement.getAttribute('data-ListType') == 'pattern'){
             patternFromList.innerText=calcList[i].innerText;
             patternFromListVal.innerText = patternOfChair[calcList[i].innerText];
@@ -85,5 +97,19 @@ for (let i = 0; i < calcList.length; i++) {
             materialFromList.innerText = calcList[i].innerText;
             materialFromListVal.innerText = materialOfChair[calcList[i].innerText];
         }
+        calcList[i].parentElement.classList.remove('showHidden');
+        sumAll(values,finalResult);
     });
 };
+
+transport.addEventListener('click', () =>{
+    
+    if(transportVal.innerText == '200'){
+        transportVal.innerText = '';
+        transportPanelL.innerText = '';
+    }else{
+        transportVal.innerText = 200;
+        transportPanelL.innerText = 'Transport';
+    }
+    sumAll(values, finalResult);
+});
